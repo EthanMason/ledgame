1df39dabe6df89ff0004d2e89406a1eeddf6434c
ethan <ethan@debian>
2020-09-17 16:06:12 -0400

    intial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: ethan
+ *
+ * Created on September 17, 2020, 3:58 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
