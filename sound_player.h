9c4427369efabca91d2033eb2ef3cd5f9fa30198
ethan <ethan@debian>
2020-12-15 07:30:22 -0500

    Fixing gitissue

--- sound_player.h
+++ sound_player.h
@@ -11,9 +11,17 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
+    enum sounds {start_up, music, generating, button_1, button_2, button_3, button_4};
+    
+    
     //24 bit address input parameter
-    void play_sound(long long address_input,long size);
+    
+    
+    
+    
+    void play_sound(enum sounds);
     void intialize_player();
+    _Bool done_playing();
 
 #ifdef	__cplusplus
 }
